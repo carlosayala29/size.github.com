c1e3c1de28946cffed83ddc0e504fb805ea5c506
carlosayala29 <130587667+carlosayala29@users.noreply.github.com>
2023-07-02 21:54:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,28 @@ $(document).ready(function() {
       $("#main-container-2").css("display", "none");
     });
   });
- document.addEventListener('DOMContentLoaded', function() {
-    var inputElement = document.getElementById('#input');
+
+document.addEventListener('DOMContentLoaded', function() {
+    var inputElement = document.getElementById('input');
   
     inputElement.addEventListener('input', function(event) {
       var inputValue = event.target.value;
       var cleanedValue = inputValue.replace(/\D/g, '');
       event.target.value = cleanedValue;
+    });
+  
+    inputElement.addEventListener('keydown', function(event) {
+      // Get the key code of the pressed key
+      var keyCode = event.keyCode || event.which;
+      
+      // Allow navigation keys and the backspace key
+      if (keyCode === 9 || keyCode === 37 || keyCode === 39 || keyCode === 8) {
+        return;
+      }
+      
+      // Prevent non-numeric values
+      if (keyCode < 48 || keyCode > 57) {
+        event.preventDefault();
+      }
     });
   });
